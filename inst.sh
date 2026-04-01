9b143b9f050685ba50b3acac8d544142fe64a0cb
nerioff1337 <89739580+nerioff1337@users.noreply.github.com>
2026-04-01 21:43:15 +0300

    Update inst.sh

--- inst.sh
+++ inst.sh
@@ -18,7 +18,7 @@ fi
 
 # 2. Твои данные
 RAW_SECRET="66201f1254d49da996da7be6f48bf4e9"
-DOMAIN_HEX="676f6f676c652e636f6d" # google.com
+DOMAIN_HEX="7275747562652e7275" # rutube.ru
 FULL_SECRET="ee${RAW_SECRET}${DOMAIN_HEX}"
 CONF_PATH="/tmp/mtg.toml"
 
